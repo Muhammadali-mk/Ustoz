2e84a5cf9cb877f3550b52efb7c7c2db37268a0c
Muhammadali Rasuljonov <muhammadali.rasuljonov223@gmail.com>
2020-07-29 00:45:42 +0500

    horizontal recyclerview fixed

--- data/src/main/java/e/ustoz/data/repository/course/CourseRepositoryImpl.kt
+++ data/src/main/java/e/ustoz/data/repository/course/CourseRepositoryImpl.kt
@@ -7,6 +7,6 @@ import kotlinx.coroutines.flow.flowOf
 class CourseRepositoryImpl : CourseRepository {
 
     override fun getOnlineCources(): Flow<List<OnlineLessonResponse>> {
-        return flowOf(listOf(OnlineLessonResponse(2,"Базовый курс по структурам данных"),OnlineLessonResponse(3,"Проектирование "),OnlineLessonResponse(4,"Математика"),OnlineLessonResponse(5,"Математика"),OnlineLessonResponse(6,"FIVE"),OnlineLessonResponse(7,"физика"),OnlineLessonResponse(7,"философия"),OnlineLessonResponse(8,"Русский язык 1"),OnlineLessonResponse(9,"Русский язык 2")))
+        return flowOf(listOf(OnlineLessonResponse(2,"Базовый курс по структурам данных"),OnlineLessonResponse(3,"Проектирование механических "),OnlineLessonResponse(4,"Математика"),OnlineLessonResponse(5,"Математика"),OnlineLessonResponse(6,"FIVE"),OnlineLessonResponse(7,"физика"),OnlineLessonResponse(7,"философия"),OnlineLessonResponse(8,"Русский язык 1"),OnlineLessonResponse(9,"Русский язык 2")))
     }
 }
